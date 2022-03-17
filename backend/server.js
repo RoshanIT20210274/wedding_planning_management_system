925ba1cc497881fa4ae8642fd92dfc76085bc5b9
IT20193522 <it20193522@my.sliit.lk>
2022-03-17 13:31:35 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+//reading url in .env file
+require("dotenv").config();
+
+//Define port
+const PORT = process.env.PORT || 8070;
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+//Connect database
+
+const URL = process.env.MONGODB_URL;
+mongoose.connect(URL, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+//open created database connection
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("mongodb Connection success");
+});
+//running port 8970
+app.listen(PORT, () => {
+  console.log(`Server is up and running on port no: ${PORT}`);
+});
